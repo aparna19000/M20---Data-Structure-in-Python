4d875cc8f4c6c0478c186269b886dd9d472bf350
aparna19000 <54104999+aparna19000@users.noreply.github.com>
2025-09-30 00:32:44 +0530

    Update file5.py

---
+++ GETTING STARTED WITH LISTS/file5.py
@@ -0,0 +1,8 @@
+# Example string
+text = "apple banana cherry"
+
+# Split the string into a list using space as separator
+fruits = text.split()
+
+print("Original string:", text)
+print("List after split:", fruits)
